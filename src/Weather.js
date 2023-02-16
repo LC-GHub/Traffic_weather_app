db8011263f83dc0446ac2ce950d23911d31518fa
LC-GHub <2717891c@student.gla.ac.uk>
2023-02-16 17:50:34 +0800

    Added: Screenshot, location list and weather

--- src/Weather.js
+++ src/Weather.js
@@ -4,12 +4,12 @@ function Weather(props) {
     const dateFormatted = `${props.date.$y}-${(props.date.$M < 10 ? '0' : '') + props.date.$M}-${(props.date.$D < 10 ? '0' : '') + props.date.$D}`;
     const hourFormatted = `${(props.time.$H < 10 ? '0' : '') + props.time.$H}`
     const minFormatted = `${(props.time.$m < 10 ? '0' : '') + props.time.$m}`
-    const secFormatted = `${(props.time.$s < 10 ? '0' : '') + props.time.$s}`
+    console.log(dateFormatted)
   return (
     <div>
         <div>Weather data: {props.weather}</div>
-        <div>on date: {dateFormatted}</div>
-        <div>at time: {hourFormatted}:{minFormatted}</div>
+        <div>on date: {dateFormatted == 'NaN-NaN-NaN'? '' : dateFormatted}</div>
+        <div>at time: {isNaN(hourFormatted) ? '' : hourFormatted}:{isNaN(minFormatted) ? '' : minFormatted}</div>
     </div>
     
   )
